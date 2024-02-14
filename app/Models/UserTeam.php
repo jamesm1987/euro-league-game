31093a86f23b3b9b8cc33def09b6a751d29843ac
James McCormack <james@wirebox.co.uk>
2024-02-14 18:06:57 +0000

    userTeam

--- app/Models/UserTeam.php
+++ app/Models/UserTeam.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class UserTeam extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
